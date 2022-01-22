da99814033ffceac5bfdcef343ff2fd7d9e666c0
Calvin Remsburg <cremsburg.dev@gmail.com>
2022-01-22 23:32:03 +0000

    move inventory to file

--- files/python/download.py
+++ files/python/download.py
@@ -1,18 +1,9 @@
 from jnpr.junos import Device
 from lxml import etree
 
-inventory = [
-    {"id": "11", "device": "houston"},
-    {"id": "12", "device": "dallas"},
-    {"id": "13", "device": "amarillo"},
-    {"id": "14", "device": "sanantonio"},
-    {"id": "15", "device": "austin"},
-    {"id": "16", "device": "fortworth"},
-    {"id": "17", "device": "elpaso"},
-    {"id": "18", "device": "galveston"},
-]
+from inventory import routers
 
-for each in inventory:
+for each in routers:
     dev = Device(
         host=f"192.168.110.{each['id']}",
         user="root",
---
+++ files/python/inventory.py
@@ -0,0 +1,10 @@
+routers = [
+    {"id": "11", "device": "houston"},
+    {"id": "12", "device": "dallas"},
+    {"id": "13", "device": "amarillo"},
+    {"id": "14", "device": "sanantonio"},
+    {"id": "15", "device": "austin"},
+    {"id": "16", "device": "fortworth"},
+    {"id": "17", "device": "elpaso"},
+    {"id": "18", "device": "galveston"},
+]

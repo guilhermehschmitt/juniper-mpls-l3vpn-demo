f35dc653e08cc12c708d8dc6b8cf6c15e2bd83c5
Calvin Remsburg <cremsburg.dev@gmail.com>
2022-01-22 22:12:08 +0000

    add poor mans automation

---
+++ files/shell/download.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+scp root@192.168.110.11:/var/tmp/houston.conf ./files/junos
+scp root@192.168.110.12:/var/tmp/dallas.conf ./files/junos
+scp root@192.168.110.13:/var/tmp/amarillo.conf ./files/junos
+scp root@192.168.110.14:/var/tmp/sanantonio.conf ./files/junos
+scp root@192.168.110.15:/var/tmp/austin.conf ./files/junos
+scp root@192.168.110.16:/var/tmp/fortworth.conf ./files/junos
+scp root@192.168.110.17:/var/tmp/elpaso.conf ./files/junos
+scp root@192.168.110.18:/var/tmp/galveston.conf ./files/junos

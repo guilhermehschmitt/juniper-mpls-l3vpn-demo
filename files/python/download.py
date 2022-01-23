40ddcc1aeaa8c1a6f314423b88507b48b4fd4607
Calvin Remsburg <cremsburg.dev@gmail.com>
2022-01-23 00:57:09 +0000

    download both set and text format

--- files/python/download.py
+++ files/python/download.py
@@ -3,6 +3,8 @@ from lxml import etree
 
 from inventory import routers
 
+CONFIG_PATH = "../junos/downloaded"
+
 for each in routers:
     dev = Device(
         host=f"192.168.110.{each['id']}",
@@ -16,9 +18,11 @@ for each in routers:
 
     for each_format in formats:
         configuration = dev.rpc.get_config(options={"format": each_format})
-        f = open(f"../junos/downloaded/{each['device']}_{each_format}.conf", "w")
-        f.write(etree.tostring(configuration).decode("utf-8"))
-
-    f.close()
+        local_file = open(
+            f"{CONFIG_PATH}/{each['device']}.{each_format}.conf",
+            "w",
+        )
+        local_file.write(etree.tostring(configuration).decode("utf-8"))
+        local_file.close()
 
     print(f"downloaded: {each['device']}")

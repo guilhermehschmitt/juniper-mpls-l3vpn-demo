84c38b7354c7d9dc823ea0c38dff087371e1f052
Calvin Remsburg <cremsburg.dev@gmail.com>
2022-01-26 11:21:11 +0000

    update management prefix to align with JCL

--- files/shell/upload.sh
+++ files/shell/upload.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-sshpass -p "juniper123" scp ../junos/generated/pe1.conf automation@192.168.110.11:/var/tmp/
-sshpass -p "juniper123" scp ../junos/generated/p1.conf automation@192.168.110.12:/var/tmp/
-sshpass -p "juniper123" scp ../junos/generated/pe2.conf automation@192.168.110.13:/var/tmp/
-sshpass -p "juniper123" scp ../junos/generated/pe3.conf automation@192.168.110.14:/var/tmp/
-sshpass -p "juniper123" scp ../junos/generated/p2.conf automation@192.168.110.15:/var/tmp/
-sshpass -p "juniper123" scp ../junos/generated/pe4.conf automation@192.168.110.16:/var/tmp/
-sshpass -p "juniper123" scp ../junos/generated/ce2.conf automation@192.168.110.17:/var/tmp/
-sshpass -p "juniper123" scp ../junos/generated/ce1.conf automation@192.168.110.18:/var/tmp/
+sshpass -p "juniper123" scp ../junos/generated/pe1.conf automation@100.123.0.11:/var/tmp/
+sshpass -p "juniper123" scp ../junos/generated/p1.conf automation@100.123.0.12:/var/tmp/
+sshpass -p "juniper123" scp ../junos/generated/pe2.conf automation@100.123.0.13:/var/tmp/
+sshpass -p "juniper123" scp ../junos/generated/pe3.conf automation@100.123.0.14:/var/tmp/
+sshpass -p "juniper123" scp ../junos/generated/p2.conf automation@100.123.0.15:/var/tmp/
+sshpass -p "juniper123" scp ../junos/generated/pe4.conf automation@100.123.0.16:/var/tmp/
+sshpass -p "juniper123" scp ../junos/generated/ce2.conf automation@100.123.0.17:/var/tmp/
+sshpass -p "juniper123" scp ../junos/generated/ce1.conf automation@100.123.0.18:/var/tmp/
